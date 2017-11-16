27146b062e72d72954c824685b37c976526714d8
leonng <ngyl-wa15@student.tarc.edu.my>
2017-11-16 12:00:05 +0800

    initial

---
+++ app/src/main/java/my/edu/taruc/myapplication/aboutactivity.java
@@ -0,0 +1,17 @@
+package my.edu.taruc.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class aboutactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        WebView webViewAbout=(WebView)findViewById(R.id.webViewAbout);
+        webViewAbout.loadUrl("http://bait2073.000webhostapp.com/welcome.html");
+    }
+}
